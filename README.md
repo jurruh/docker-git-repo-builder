d27cef792bd6b1359fffe1a54aa66b8aafc86b89
Jurre <jurre.vriesen@skyberate.nl>
2019-10-14 11:41:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Docker git repo builder
+
+## Example request body
+```json
+{
+    "repository": "https://github.com/jurruh/example-docker-image.git",
+    "tag": "jurruh/example-docker-image:latest",
+    "registry": "5da427c302653c15c6d5d229.test.jurrevriesen.nl" 
+}
+```
---
+++ src/index.js
@@ -0,0 +1,70 @@
+const Docker = require('dockerode');
+const docker = new Docker({ socketPath: process.env.DOCKER_SOCK || '/var/run/docker.sock' });
+const simpleGit = require('simple-git/promise')(process.env.CLONE_PATH || '/tmp');
+const tar = require('tar-fs');
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json());
+
+app.post('', async (req, res) => {
+    const { repository, tag, registry } = req.body;
+    const cloneDest = `${Math.random().toString(36).substr(2, 9)}/${tag}`;
+
+    if (!repository || !tag || !registry) return res.status(400).send();
+
+    const log = [];
+
+    try {
+        await simpleGit.clone(repository, cloneDest);
+        tarStream = tar.pack(`/tmp/${cloneDest}`);
+
+        let stream = await docker.buildImage(tarStream, { t: `${registry}/${tag}` });
+        await new Promise((resolve, reject) => {
+            const onFinished = (err, output) => {
+                err ? reject() : resolve(output)
+                console.log(output);
+            }
+
+            const onProgress = (event) => {
+                log.push(event.stream);
+            }
+
+            docker.modem.followProgress(stream, onFinished, onProgress);
+        });
+
+        const image = docker.getImage(`${registry}/${tag}`);
+
+        await new Promise((resolve, reject) => {
+            image.push({}, (err, stream) => {
+
+                const onFinished = (err, output) => {
+                    err ? reject() : resolve(output)
+                    console.log(output);
+                }
+
+                const onProgress = (event) => {
+                    log.push(JSON.stringify(event));
+                    console.log(event);
+                }
+
+                docker.modem.followProgress(stream, onFinished, onProgress);
+            }, {});
+        });
+
+        res.json(
+            {
+                image: `${registry}/${tag}`,
+                log: log,
+            }
+        );
+    } catch (error) {
+        res.status(500).send({
+            error,
+            log,
+        });
+    }
+})
+
+app.listen(3005);
